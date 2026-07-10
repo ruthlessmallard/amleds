df9926bb348eb7cf487f95abb2841a7093490f0a
Shawn <shawn@solarcargocompany.game>
2026-07-09 21:53:12 -0600

    Fix: Remove unnecessary hide directive from theme import

--- lib/widgets/status_indicator.dart
+++ lib/widgets/status_indicator.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/ping_result.dart';
-import '../theme/retro_terminal_theme.dart' hide PingStatus;
+import '../theme/retro_terminal_theme.dart';
 
 /// Vitals-style status indicator for ping results
 class StatusIndicator extends StatelessWidget {
